50ed59b0a38ba9cb1fc4899cc43592475af79bb3
Noisyninja703 <sivan.moodley02@gmail.com>
2024-01-02 13:36:36 +0200

    Added bot brain

--- Blackjack_Cabinet.py
+++ Blackjack_Cabinet.py
@@ -21,6 +21,7 @@
 # ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿Sivan Moodley - NNZA GitHub⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 # ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 import random
+import pandas as pd
 
 #region Game Mechanics
 def check_ace(hand): 
@@ -56,7 +57,6 @@ def create_deck(num_of_decks=1):
     deck = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']*4*num_of_decks
     random.shuffle(deck)
     return deck
-
 
 def player_print(hand, total): 
     """
@@ -267,6 +267,38 @@ def play_blackjack():
             print("Not enough cards left. Game over.")
             break
         play = play_again()
+#endregion
+        
+#region Card Counting matrix
+def create_cc_matrix():
+    # Creating a simple dictionary to start the values
+    vals = {'2': 1, '3': 1, '4': 1, '5': 1, '6': 1, 
+                '7': 0, '8': 0, '9': 0, '10': -1, 'J': -1, 
+                'Q': -1, 'K': -1, 'A': -1, 'A.': -1}
+
+    # Converting to a DataFrame
+    df = pd.DataFrame(vals, index=[0])
+
+    # Adding in values to each row in the DF
+    df.loc[len(df), :] = [0,1,1,1,1,0,0,0,-1,-1,-1,-1,0,0]
+    df.loc[len(df), :] = [1,1,2,2,1,1,0,0,-2,-2,-2,-2,0,0]
+    df.loc[len(df), :] = [1,1,1,1,1,1,0,0,-1,-1,-1,-1,-1,-1]
+    df.loc[len(df), :] = [1,1,2,2,2,1,0,-1,-2,-2,-2,-2,0,0]
+    df.loc[len(df), :] = [1,1,1,1,1,0,0,0,-1,-1,-1,-1,-1,-1]
+    df.loc[len(df), :] = [.5,1,1,1.5,1,.5,0,-.5,-1,-1,-1,-1,-1,-1]
+    df.loc[len(df), :] = [1,1,2,2,2,1,0,0,-2,-2,-2,-2,-1,-1]
+    df.loc[len(df), :] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+    # Renaming the index for each row
+    df.rename({0: 'Hi-Lo',
+            1: 'Hi-Opt I',
+            2: 'Hi-Opt II',
+            3: 'KO',
+            4: 'Omega II',
+            5: 'Red 7',
+            6: 'Halves',
+            7: 'Zen Count',
+            8: 'No Strategy'}, inplace=True)
 #endregion
 
 #region Start Game
